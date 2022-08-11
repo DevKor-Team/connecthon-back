0f0daff577ceec5c65bb1f88d8d38393fbc4f07c
overthestream <njhnjh02@naver.com>
2022-08-12 00:24:30 +0900

    fix redirect url

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,3 @@
-export const LOGIN_SUCCESS_REDIRECT = '/';
-export const LOGIN_FAILURE_REDIRECT = '/login';
+export const LOGIN_SUCCESS_REDIRECT = 'https://connecthon.com';
+export const LOGIN_FAILURE_REDIRECT = 'https://connecthon.com/login';
 export const ADMIN_LEVEL = 2;
